33462f875fd379e624ca2c584b7b9750ecf94888
Oriol Canadés <oriol.canades@in2.es>
2024-01-25 09:14:52 +0100

    Project setup

---
+++ src/main/java/es/in2/desmos/DesmosApplication.java
@@ -0,0 +1,13 @@
+package es.in2.desmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmosApplication.class, args);
+	}
+
+}
