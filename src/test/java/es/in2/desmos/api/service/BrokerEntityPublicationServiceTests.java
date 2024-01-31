bbeff38be25ebf295126d8b38f9cb252752d32ca
Juan Martin Gallo <juan.gallo@in2.es>
2024-01-31 11:10:23 +0100

    Update BrokerEntityPublicationServiceTests.java

--- src/test/java/es/in2/desmos/api/service/BrokerEntityPublicationServiceTests.java
+++ src/test/java/es/in2/desmos/api/service/BrokerEntityPublicationServiceTests.java
@@ -2,15 +2,20 @@ package es.in2.desmos.api.service;
 
 import es.in2.desmos.api.model.BlockchainNotification;
 import es.in2.desmos.api.service.impl.BrokerEntityPublicationServiceImpl;
+import es.in2.desmos.api.util.ApplicationUtils;
 import es.in2.desmos.broker.service.BrokerPublicationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,22 +25,28 @@ class BrokerEntityPublicationServiceTests {
     private TransactionService transactionService;
     @Mock
     private BrokerPublicationService brokerPublicationService;
-    @Mock
+    @InjectMocks
     private BrokerEntityPublicationServiceImpl brokerEntityPublicationService;
 
     @Test
     void testDeletedEntityNotification() {
+        // Arrange
         String processId = "process123";
         String entityId = "entity123";
         BlockchainNotification notification = BlockchainNotification.builder()
                 .dataLocation("http://broker.internal/entities/entity123")
                 .build();
-
         when(brokerPublicationService.deleteEntityById(processId, entityId)).thenReturn(Mono.empty());
         when(transactionService.saveTransaction(any(), any())).thenReturn(Mono.empty());
-
-        StepVerifier.create(brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, "{errorCode: 404}", notification))
-                .verifyComplete();
+        try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
+            applicationUtils
+                    .when(() -> ApplicationUtils.extractEntityIdFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            // Act & Assert
+            StepVerifier.create(
+                            brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, "{errorCode: 404}", notification))
+                    .verifyComplete();
+        }
     }
 
 }
