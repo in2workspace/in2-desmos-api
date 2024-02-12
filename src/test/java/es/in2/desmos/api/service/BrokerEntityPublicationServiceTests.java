c815e3f73e60997d39323e5a7beedd012502fbd1
Juan Martin Gallo <juan.gallo@in2.es>
2024-02-12 16:27:52 +0100

    Update BrokerEntityPublicationServiceTests.java

--- src/test/java/es/in2/desmos/api/service/BrokerEntityPublicationServiceTests.java
+++ src/test/java/es/in2/desmos/api/service/BrokerEntityPublicationServiceTests.java
@@ -4,6 +4,7 @@ import es.in2.desmos.api.model.BlockchainNotification;
 import es.in2.desmos.api.service.impl.BrokerEntityPublisherServiceImpl;
 import es.in2.desmos.api.util.ApplicationUtils;
 import es.in2.desmos.broker.service.BrokerPublicationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.security.NoSuchAlgorithmException;
+
+import static es.in2.desmos.api.util.ApplicationUtils.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -21,6 +25,9 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class BrokerEntityPublicationServiceTests {
 
+    String processId;
+    String entityId;
+    BlockchainNotification notification;
     @Mock
     private TransactionService transactionService;
     @Mock
@@ -28,19 +35,24 @@ class BrokerEntityPublicationServiceTests {
     @InjectMocks
     private BrokerEntityPublisherServiceImpl brokerEntityPublicationService;
 
+    @BeforeEach
+    void setUp() {
+        processId = "process123";
+        entityId = "entity123";
+        notification = BlockchainNotification.builder()
+                .dataLocation("http://broker.internal/entities/entity123")
+                .build();
+    }
+
+
     @Test
     void testDeletedEntityNotification() {
         // Arrange
-        String processId = "process123";
-        String entityId = "entity123";
-        BlockchainNotification notification = BlockchainNotification.builder()
-                .dataLocation("http://broker.internal/entities/entity123")
-                .build();
         when(brokerPublicationService.deleteEntityById(processId, entityId)).thenReturn(Mono.empty());
         when(transactionService.saveTransaction(any(), any())).thenReturn(Mono.empty());
         try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
             applicationUtils
-                    .when(() -> ApplicationUtils.extractEntityIdFromDataLocation(anyString()))
+                    .when(() -> extractEntityIdFromDataLocation(anyString()))
                     .thenReturn(entityId);
             // Act & Assert
             StepVerifier.create(
@@ -49,4 +61,91 @@ class BrokerEntityPublicationServiceTests {
         }
     }
 
+    @Test
+    void testNotDeletedEntityNotification() {
+        // Arrange
+        String retrievedBrokerEntity = "brokerEntity";
+        when(brokerPublicationService.getEntityById(processId, entityId)).thenReturn(Mono.just("{errorCode: 404}"));
+        when(brokerPublicationService.postEntity(processId, retrievedBrokerEntity)).thenReturn(Mono.empty());
+        when(transactionService.saveTransaction(any(), any())).thenReturn(Mono.empty());
+        try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
+            applicationUtils
+                    .when(() -> extractEntityIdFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> calculateSHA256Hash(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> extractEntityHashFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            // Act & Assert
+            StepVerifier.create(
+                            brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, retrievedBrokerEntity, notification))
+                    .verifyComplete();
+        }
+    }
+
+    @Test
+    void testValidEntityIntegrity() {
+        // Arrange
+        String retrievedBrokerEntity = "brokerEntity";
+        when(brokerPublicationService.getEntityById(processId, entityId)).thenReturn(Mono.just("Ok"));
+        when(brokerPublicationService.updateEntity(processId, retrievedBrokerEntity)).thenReturn(Mono.empty());
+        when(transactionService.saveTransaction(any(), any())).thenReturn(Mono.empty());
+        try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
+            applicationUtils
+                    .when(() -> extractEntityIdFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> calculateSHA256Hash(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> extractEntityHashFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            // Act & Assert
+            StepVerifier.create(
+                            brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, retrievedBrokerEntity, notification))
+                    .verifyComplete();
+        }
+    }
+
+    @Test
+    void testInvalidEntityIntegrity() {
+        // Arrange
+        String retrievedBrokerEntity = "brokerEntity";
+
+        try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
+            applicationUtils
+                    .when(() -> extractEntityIdFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> calculateSHA256Hash(anyString()))
+                    .thenReturn("entity1");
+            applicationUtils
+                    .when(() -> extractEntityHashFromDataLocation(anyString()))
+                    .thenReturn("entity2");
+            // Act & Assert
+            StepVerifier.create(
+                            brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, retrievedBrokerEntity, notification))
+                    .verifyError(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void testCatchException() {
+        // Arrange
+        String retrievedBrokerEntity = "brokerEntity";
+        try (MockedStatic<ApplicationUtils> applicationUtils = Mockito.mockStatic(ApplicationUtils.class)) {
+            applicationUtils
+                    .when(() -> extractEntityIdFromDataLocation(anyString()))
+                    .thenReturn(entityId);
+            applicationUtils
+                    .when(() -> calculateSHA256Hash(anyString()))
+                    .thenThrow(NoSuchAlgorithmException.class);
+            // Act & Assert
+            StepVerifier.create(
+                            brokerEntityPublicationService.publishRetrievedEntityToBroker(processId, retrievedBrokerEntity, notification))
+                    .verifyError(NoSuchAlgorithmException.class);
+        }
+    }
 }
