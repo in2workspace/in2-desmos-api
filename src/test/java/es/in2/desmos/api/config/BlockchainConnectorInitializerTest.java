80a30372956deb97c5ecb9cb29e9abd6b1891e2c
yaniaiciin2 <yani.aici@in2.es>
2024-02-15 11:23:04 +0100

    Update BlockchainConnectorInitializerTest.java

--- src/test/java/es/in2/desmos/api/config/BlockchainConnectorInitializerTest.java
+++ src/test/java/es/in2/desmos/api/config/BlockchainConnectorInitializerTest.java
@@ -20,7 +20,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 import java.sql.Timestamp;
@@ -158,61 +160,82 @@ class BlockchainConnectorInitializerTest {
 
     }
 
-//    @Test
-//    void testGetPreviousTransactions() {
-//        // Given
-//        Transaction lastTransactionPublished = Transaction.builder()
-//                .transactionId("e1e07f6d-e8e7-48ae-bb4d-afab5b63c1f5")
-//                .createdAt(Timestamp.from(Instant.now()))
-//                .dataLocation("https://domain.org/ngsi-ld/v1/entities/urn:ngsi-ld:Entity:1234")
-//                .entityId("urn:ngsi-ld:Entity:1234")
-//                .entityType("Entity")
-//                .entityHash("0x1234")
-//                .status(TransactionStatus.PUBLISHED)
-//                .trader(TransactionTrader.CONSUMER)
-//                .hash("0x9876")
-//                .build(); // Populate this as needed
-//        Transaction lastTransactionCreated = Transaction.builder()
-//                .transactionId("e1e07f6d-e8e7-48ae-bb4d-afab5b63c1f6")
-//                .createdAt(Timestamp.from(Instant.now()))
-//                .dataLocation("https://domain.org/ngsi-ld/v1/entities/urn:ngsi-ld:Entity:12345")
-//                .entityId("urn:ngsi-ld:Entity:12345")
-//                .entityType("Entity")
-//                .entityHash("0x1235")
-//                .status(TransactionStatus.CREATED)
-//                .trader(TransactionTrader.CONSUMER)
-//                .hash("0x9876")
-//                .build();
-//        lastTransactionPublished.setCreatedAt(Timestamp.valueOf("2024-02-06 10:07:01.529"));
-//        List<Transaction> transactionList = List.of(lastTransactionPublished, lastTransactionCreated);
-//
-//        Flux<Transaction> transactionsFlux = Flux.fromIterable(transactionList);
-//
-//        // Assuming queryDLTAdapterFromRange returns an empty Flux for simplicity
-//
-//        when(transactionService.getAllTransactions(any())).thenReturn(transactionsFlux);
-////        when(webClientMock.get()).thenReturn(requestHeadersUriSpecMock);
-//        when(requestHeadersUriSpecMock.uri((String) org.mockito.ArgumentMatchers.any())).thenReturn(requestHeadersSpecMock);
-//        when(requestHeadersSpecMock.accept((org.springframework.http.MediaType) org.mockito.ArgumentMatchers.any())).thenReturn
-//        (requestHeadersSpecMock);
-//        when(requestHeadersSpecMock.retrieve()).thenReturn(responseSpecMock);
-//
-//
-//        String dltNotificationDTOexample = "{\"id\":1,\"publisherAddress\":\"String\",\"eventType\":\"ProductOffering\"," +
-//                "\"timestamp\":3,\"dataLocation\":\"http://scorpio:9090/ngsi-ld/v1/entities/urn:ngsi-ld:product-offering:443734333"
-//                +
-//                "?hl=0xd6e502951d411812220f92c9eb3795eb2674aa71918128f19daf296deb40942\",\"relevantMetadata\":[]}";
-//        when(blockchainAdapterEventPublisher.getEventsFromRange(any(String.class), any(Long.class), any(Long.class)))
-//                .thenReturn(Flux.just(dltNotificationDTOexample));
-//        Flux<String> mockResponse = Flux.just(dltNotificationDTOexample);
-//        when(responseSpecMock.bodyToFlux(String.class)).thenReturn(mockResponse);
-//
-//        // When
-//        blockchainConnectorInitializer.processAllTransactions();
-//
-//        // Then
-//        verify(transactionService, times(1)).getAllTransactions(any());
-//    }
+    @Test
+    void testGetPreviousTransactions() {
+        // Given
+        Transaction lastTransactionPublished = Transaction.builder()
+                .transactionId("e1e07f6d-e8e7-48ae-bb4d-afab5b63c1f5")
+                .createdAt(Timestamp.from(Instant.now()))
+                .dataLocation("https://domain.org/ngsi-ld/v1/entities/urn:ngsi-ld:Entity:1234")
+                .entityId("urn:ngsi-ld:Entity:1234")
+                .entityType("Entity")
+                .entityHash("0x1234")
+                .status(TransactionStatus.PUBLISHED)
+                .trader(TransactionTrader.CONSUMER)
+                .hash("0x9876")
+                .build(); // Populate this as needed
+        Transaction lastTransactionCreated = Transaction.builder()
+                .transactionId("e1e07f6d-e8e7-48ae-bb4d-afab5b63c1f6")
+                .createdAt(Timestamp.from(Instant.now()))
+                .dataLocation("https://domain.org/ngsi-ld/v1/entities/urn:ngsi-ld:Entity:12345")
+                .entityId("urn:ngsi-ld:Entity:12345")
+                .entityType("Entity")
+                .entityHash("0x1235")
+                .status(TransactionStatus.CREATED)
+                .trader(TransactionTrader.CONSUMER)
+                .hash("0x9876")
+                .build();
+        lastTransactionPublished.setCreatedAt(Timestamp.valueOf("2024-02-06 10:07:01.529"));
+        List<Transaction> transactionList = List.of(lastTransactionPublished, lastTransactionCreated);
+
+        Flux<Transaction> transactionsFlux = Flux.fromIterable(transactionList);
+
+        // Assuming queryDLTAdapterFromRange returns an empty Flux for simplicity
+
+        when(transactionService.getAllTransactions(any())).thenReturn(transactionsFlux);
+//        when(webClientMock.get()).thenReturn(requestHeadersUriSpecMock);
+        when(requestHeadersUriSpecMock.uri((String) org.mockito.ArgumentMatchers.any())).thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.accept((org.springframework.http.MediaType) org.mockito.ArgumentMatchers.any())).thenReturn
+        (requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve()).thenReturn(responseSpecMock);
+
+
+        String dltNotificationDTOexample = "{\"id\":1,\"publisherAddress\":\"String\",\"eventType\":\"ProductOffering\"," +
+                "\"timestamp\":3,\"dataLocation\":\"http://scorpio:9090/ngsi-ld/v1/entities/urn:ngsi-ld:product-offering:443734333"
+                +
+                "?hl=0xd6e502951d411812220f92c9eb3795eb2674aa71918128f19daf296deb40942\",\"relevantMetadata\":[]}";
+        when(blockchainAdapterEventPublisher.getEventsFromRange(any(String.class), any(Long.class), any(Long.class)))
+                .thenReturn(Flux.just(dltNotificationDTOexample));
+        Flux<String> mockResponse = Flux.just(dltNotificationDTOexample);
+        when(responseSpecMock.bodyToFlux(String.class)).thenReturn(mockResponse);
+
+        // When
+        blockchainConnectorInitializer.processAllTransactions();
+
+        // Then
+        verify(transactionService, times(1)).getAllTransactions(any());
+    }
+
+    @Test
+    void cleanUpShouldDisposeResources() {
+        // Simula suscripciones activas
+        Disposable blockchainEventSubscription = mock(Disposable.class);
+        Disposable brokerEntityEventSubscription = mock(Disposable.class);
+        when(blockchainEventSubscription.isDisposed()).thenReturn(false);
+        when(brokerEntityEventSubscription.isDisposed()).thenReturn(false);
+
+        // Asigna las suscripciones simuladas
+        ReflectionTestUtils.setField(blockchainConnectorInitializer, "blockchainEventProcessingSubscription", blockchainEventSubscription);
+        ReflectionTestUtils.setField(blockchainConnectorInitializer, "brokerEntityEventProcessingSubscription", brokerEntityEventSubscription);
+
+        // Ejecuta cleanUp
+        blockchainConnectorInitializer.cleanUp();
+
+        // Verificaciones
+        verify(blockchainEventSubscription).dispose();
+        verify(brokerEntityEventSubscription).dispose();
+    }
+
 
 
 }
