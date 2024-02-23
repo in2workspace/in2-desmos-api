14eb00d71461b07367d145830cbf72f617b6896b
yaniaiciin2 <yani.aici@in2.es>
2024-02-23 12:34:07 +0100

    Update BlockchainEventCreatorServiceImpl.java

--- src/main/java/es/in2/desmos/api/service/impl/BlockchainEventCreatorServiceImpl.java
+++ src/main/java/es/in2/desmos/api/service/impl/BlockchainEventCreatorServiceImpl.java
@@ -74,7 +74,7 @@ public class BlockchainEventCreatorServiceImpl implements BlockchainEventCreator
                                 .hash(extractEntityHashFromDataLocation(blockchainEvent.dataLocation()))
                                 .status(TransactionStatus.CREATED)
                                 .trader(TransactionTrader.PRODUCER)
-                                .hashlink(dataLocation)
+                                .hashlink(blockchainEvent.dataLocation())
                                 .newTransaction(true)
                                 .build())
                         .thenReturn(blockchainEvent);
