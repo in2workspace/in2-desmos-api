6180fc9e5f33d5365c2417c37217df245e7a0474
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-05 10:22:34 +0200

    test(objectmother): fix return and list declaration

--- src/test/java/es/in2/desmos/objectmothers/SyncDiscoveryRequestMother.java
+++ src/test/java/es/in2/desmos/objectmothers/SyncDiscoveryRequestMother.java
@@ -1,7 +1,10 @@
 package es.in2.desmos.objectmothers;
 
+import es.in2.desmos.domain.models.ProductOffering;
+import es.in2.desmos.domain.models.SyncDiscoveryRequest;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,9 +15,9 @@ public final class SyncDiscoveryRequestMother {
 
     public static @NotNull SyncDiscoveryRequest simpleSyncDiscoveryRequest() {
         String issuer = "https://my-domain.org";
-        List<ProductOffering> productOfferingList = new List<ProductOffering>();
+        List<ProductOffering> productOfferingList = new ArrayList<ProductOffering>();
         productOfferingList.add(new ProductOffering("urn:productOffering:d86735a6-0faa-463d-a872-00b97affa1cb", "1.2", "2024-04-01T12:00:00Z"));
         productOfferingList.add(new ProductOffering("urn:productOffering:ed9c56c8-a5ab-42cc-bc62-0fca69a30c87", "1.5", "2024-09-01T12:00:00Z"));
-        var syncDiscoveryRequest = new SyncDiscoveryRequest(issuer, productOfferingList);
+        return new SyncDiscoveryRequest(issuer, productOfferingList);
     }
 }
