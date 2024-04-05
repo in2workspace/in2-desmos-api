cfbc9e3a071017688687f061367f15650d7284f6
yaniaiciin2 <yani.aici@in2.es>
2024-04-05 10:53:50 +0200

    Update BlockchainTxPayloadFactoryTests.java

--- src/test/java/es/in2/desmos/domain/utils/BlockchainTxPayloadFactoryTests.java
+++ src/test/java/es/in2/desmos/domain/utils/BlockchainTxPayloadFactoryTests.java
@@ -1,11 +1,13 @@
 package es.in2.desmos.domain.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import es.in2.desmos.configs.ApiConfig;
 import es.in2.desmos.configs.BrokerConfig;
 import es.in2.desmos.domain.exceptions.HashLinkException;
 import es.in2.desmos.domain.models.BlockchainTxPayload;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -37,15 +39,19 @@ class BlockchainTxPayloadFactoryTests {
 
     Map<String, Object> dataMap = new HashMap<>();
 
+    @BeforeEach
+    void setUp() {
+        dataMap = new HashMap<>();
+        dataMap.put("id", "entity123");
+        dataMap.put("type", "productOffering");
+        dataMap.put("name", "Cloud Services Suite");
+        dataMap.put("description", "Example of a Product offering for cloud services suite");
+    }
 
     @Test
     void testBuildBlockchainTxPayload_validData_firstHash_Success() throws Exception {
         //Arrange
         String processId = "processId";
-        dataMap.put("id", "entity123");
-        dataMap.put("type", "productOffering");
-        dataMap.put("name", "Cloud Services Suite");
-        dataMap.put("description", "Example of a Product offering for cloud services suite");
         String previousHash = "5077272d496c8afd1af9d3740f9e5f11837089b5952d577eff4c20509e6e199e";
         when(objectMapper.writeValueAsString(dataMap)).thenReturn("dataMapString");
         when(apiConfig.organizationIdHash()).thenReturn("381d18e478b9ae6e67b1bf48c9f3bcaf246d53c4311bfe81f46e63aa18167c89");
@@ -66,9 +72,6 @@ class BlockchainTxPayloadFactoryTests {
     void testBuildBlockchainTxPayload_validData_differentHashes_Success() throws Exception {
         // Arrange
         String processId = "processId";
-        dataMap.put("id", "entity123");
-        dataMap.put("type", "productOffering");
-        dataMap.put("name", "Cloud Services Suite");
         dataMap.put("description", "Example of a Product offering for cloud services suite");
         String previousHash = "22d0ef4e87a39c52191998f4fbf32ff672f82ed5a2b4c9902371a161402a0faf";
         when(objectMapper.writeValueAsString(dataMap)).thenReturn("dataMapString");
@@ -91,37 +94,29 @@ class BlockchainTxPayloadFactoryTests {
 
     }
 
-    // WIP
     @Test
     void testBuildBlockchainTxPayload_invalidData_Failure() throws Exception {
         // Arrange
         String processId = "processId";
-        dataMap.put("id", "entity123");
-        dataMap.put("name", "Cloud Services Suite");
-        dataMap.put("description", "Example of a Product offering for cloud services suite");
         String previousHash = "22d0ef4e87a39c52191998f4fbf32ff672f82ed5a2b4c9902371a161402a0faf";
+        when(objectMapper.writeValueAsString(dataMap)).thenThrow(new JsonProcessingException("Error") {
+        });
+
 
         // Act
         Mono<BlockchainTxPayload> resultMono = blockchainTxPayloadFactory.buildBlockchainTxPayload(processId, dataMap, previousHash);
 
         // Assert
-        // Check that the previous hash is different from the hash of the data, because it is not the first hash, it is the concatenation of the previous hash and the hash of the data
         StepVerifier.create(resultMono)
-                .assertNext(blockchainTxPayload -> {
-                    Assertions.assertThrows(HashLinkException.class, () -> {
-                        ApplicationUtils.extractHashLinkFromDataLocation(blockchainTxPayload.dataLocation());
-                    });
-                }).verifyComplete();
+                .expectErrorMatches(throwable -> throwable instanceof HashLinkException && throwable.getMessage().contains("Error creating blockchain transaction payload"))
+                .verify();
     }
+
 
     @Test
     void testCalculatePreviousHashIfEmpty_validData_Success() throws Exception {
         // Arrange
         String processId = "processId";
-        dataMap.put("id", "entity123");
-        dataMap.put("type", "productOffering");
-        dataMap.put("name", "Cloud Services Suite");
-        dataMap.put("description", "Example of a Product offering for cloud services suite");
         when(objectMapper.writeValueAsString(dataMap)).thenReturn("dataMapString");
 
         // Act
@@ -139,22 +134,20 @@ class BlockchainTxPayloadFactoryTests {
                 }).verifyComplete();
     }
 
-    // WIP
     @Test
-    void testCalculatePreviousHashIfEmpty_invalidData_Failure() throws Exception {
+    void testCalculatePreviousHashIfEmpty_invalidData_Failure() throws JsonProcessingException {
         // Arrange
         String processId = "processId";
-        dataMap.put("type", "productOffering");
-        dataMap.put("id", "entity123");
-        dataMap.put("name", "Cloud Services Suite");
-        dataMap.put("description", "Example of a Product offering for cloud services suite");
-        when(objectMapper.writeValueAsString(dataMap)).thenThrow(new NoSuchAlgorithmException("Error"));
+        when(objectMapper.writeValueAsString(dataMap)).thenThrow(new JsonProcessingException("Error") {
+        });
 
         // Act
         Mono<String> resultMono = blockchainTxPayloadFactory.calculatePreviousHashIfEmpty(processId, dataMap);
 
         // Assert
-        // Check that the previous hash is the hash of the data
-        StepVerifier.create(resultMono).verifyErrorMessage("Error creating previous hash value from notification data");
+        StepVerifier.create(resultMono)
+                .expectErrorMatches(throwable -> throwable instanceof HashLinkException && throwable.getMessage().contains("Error creating previous hash value from notification data"))
+                .verify();
     }
+
 }
