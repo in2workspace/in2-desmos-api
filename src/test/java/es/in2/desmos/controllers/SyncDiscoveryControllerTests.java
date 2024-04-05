ca615fbda08d9228ea194a65de527085dd2adf2b
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-05 10:39:37 +0200

    test(controller): fix uri and added sync discovery service mocking

--- src/test/java/es/in2/desmos/controllers/SyncDiscoveryControllerTests.java
+++ src/test/java/es/in2/desmos/controllers/SyncDiscoveryControllerTests.java
@@ -2,13 +2,19 @@ package es.in2.desmos.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import es.in2.desmos.domain.services.sync.SyncDiscoveryService;
 import es.in2.desmos.objectmothers.SyncDiscoveryRequestMother;
 import es.in2.desmos.objectmothers.SyncDiscoveryResponseMother;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest(SyncDiscoveryController.class)
 class SyncDiscoveryControllerTests {
@@ -17,6 +23,9 @@ class SyncDiscoveryControllerTests {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private SyncDiscoveryService service;
 
     @Test
     void itShouldReturnExternalEntityIdsWithIssuer() throws JsonProcessingException {
@@ -27,8 +36,10 @@ class SyncDiscoveryControllerTests {
         var syncDiscoveryResponse = SyncDiscoveryResponseMother.simpleSyncDiscoveryResponse();
         var syncDiscoveryResponseJson = objectMapper.writeValueAsString(syncDiscoveryResponse);
 
+        when(service.syncDiscovery(anyString(), eq(syncDiscoveryRequest.issuer()), eq(syncDiscoveryRequest.externalEntityIds()))).thenReturn(syncDiscoveryResponse.localEntitiesIds());
+
         webTestClient.post()
-                .uri("/sync/discovery")
+                .uri("/api/v1/sync/discovery")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(syncDiscoveryRequestJson)
                 .exchange()
