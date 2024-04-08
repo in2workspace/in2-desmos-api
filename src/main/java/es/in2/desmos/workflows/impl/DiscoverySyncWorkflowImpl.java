6c9b5936cb0bf670717d2a3de959f9848e27283c
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-08 11:39:03 +0200

    feat(workflow): create discovery sync workflow

---
+++ src/main/java/es/in2/desmos/workflows/impl/DiscoverySyncWorkflowImpl.java
@@ -0,0 +1,25 @@
+package es.in2.desmos.workflows.impl;
+
+import es.in2.desmos.domain.models.ProductOffering;
+import es.in2.desmos.domain.services.sync.NewEntitiesCreatorService;
+import es.in2.desmos.workflows.DiscoverySyncWorkflow;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DiscoverySyncWorkflowImpl implements DiscoverySyncWorkflow {
+    private final NewEntitiesCreatorService newEntitiesCreatorService;
+
+    @Override
+    public List<ProductOffering> discoverySync(String processId, String issuer, List<ProductOffering> productOfferings) {
+        newEntitiesCreatorService.addNewEntities(productOfferings, issuer);
+        return getInternalProductOfferingsIds(productOfferings);
+    }
+
+
+}
