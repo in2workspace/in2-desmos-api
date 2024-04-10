3973e6812398df2768494f0c3ff84395e8419bc7
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-10 13:34:20 +0200

    fix(service): change from list to mono list

--- src/main/java/es/in2/desmos/workflows/DiscoverySyncWorkflow.java
+++ src/main/java/es/in2/desmos/workflows/DiscoverySyncWorkflow.java
@@ -1,9 +1,10 @@
 package es.in2.desmos.workflows;
 
 import es.in2.desmos.domain.models.ProductOffering;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface DiscoverySyncWorkflow {
-    List<ProductOffering> discoverySync(String processId, String issuer, List<String> externalEntityIds);
+    Mono<List<ProductOffering>> discoverySync(String processId, Mono<String> issuer, Mono<List<String>> externalEntityIds);
 }
