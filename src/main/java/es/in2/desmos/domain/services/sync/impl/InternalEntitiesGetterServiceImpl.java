f63e1c99e39d6a9655b1d37ff9365aac69cb9ccb
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-10 16:43:18 +0200

    fix(service): add service, logs and constructor annotations

--- src/main/java/es/in2/desmos/domain/services/sync/impl/InternalEntitiesGetterServiceImpl.java
+++ src/main/java/es/in2/desmos/domain/services/sync/impl/InternalEntitiesGetterServiceImpl.java
@@ -2,10 +2,16 @@ package es.in2.desmos.domain.services.sync.impl;
 
 import es.in2.desmos.domain.models.ProductOffering;
 import es.in2.desmos.domain.services.sync.InternalEntitiesGetterService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class InternalEntitiesGetterServiceImpl implements InternalEntitiesGetterService {
     @Override
     public Mono<List<ProductOffering>> getInternalEntityIds() {
