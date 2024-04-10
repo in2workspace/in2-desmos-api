ea3419de64327fc3c11cb92af738b474ceea4e2e
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-10 13:37:45 +0200

    test(service): create new entities creator service tests

---
+++ src/test/java/es/in2/desmos/domain/services/sync/NewEntitiesCreatorServiceTests.java
@@ -0,0 +1,70 @@
+package es.in2.desmos.domain.services.sync;
+
+import es.in2.desmos.domain.models.EntitySyncResponse;
+import es.in2.desmos.domain.services.sync.impl.NewEntitiesCreatorServiceImpl;
+import es.in2.desmos.objectmothers.EntitySyncResponseMother;
+import es.in2.desmos.objectmothers.ProductOfferingMother;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NewEntitiesCreatorServiceTests {
+    @InjectMocks
+    private NewEntitiesCreatorServiceImpl newEntitiesCreatorService;
+
+    @Mock
+    private InternalEntitiesGetterService internalEntitiesGetterService;
+
+    @Mock
+    private EntitySyncWebClient entitySyncWebClient;
+
+    @Test
+    void itShouldCreateNewEntitiesIfNotExists() {
+        Mono<List<String>> externalEntitiesIdList = Mono.just(createExternalEntitiesIdList());
+
+        Mono<String> issuer = Mono.just("http://example.org");
+
+        Mono<List<String>> internalEntitiesIdsMono = getInternalEntitiesIdsMono();
+        when(internalEntitiesGetterService.getInternalEntities()).thenReturn(internalEntitiesIdsMono);
+
+        Mono<EntitySyncResponse> entitySyncResponse = Mono.just(EntitySyncResponseMother.sample());
+        when(entitySyncWebClient.makeRequest(any(), any())).thenReturn(entitySyncResponse);
+
+        Mono<Void> result = newEntitiesCreatorService.addNewEntities(externalEntitiesIdList, issuer);
+
+        StepVerifier.create(result)
+                .expectComplete()
+                .verify();
+
+        verify(internalEntitiesGetterService, times(1)).getInternalEntities();
+        verifyNoMoreInteractions(internalEntitiesGetterService);
+
+        verify(entitySyncWebClient, times(1)).makeRequest(any(), any());
+        verifyNoMoreInteractions(entitySyncWebClient);
+    }
+
+    private static @NotNull Mono<List<String>> getInternalEntitiesIdsMono() {
+        List<String> internalEntitiesIds = new ArrayList<>();
+        internalEntitiesIds.add(ProductOfferingMother.sample3().id());
+        internalEntitiesIds.add(ProductOfferingMother.sample4().id());
+        return Mono.just(internalEntitiesIds);
+    }
+
+    private static @NotNull List<String> createExternalEntitiesIdList() {
+        List<String> externalEntitiesIdList = new ArrayList<>();
+        externalEntitiesIdList.add(ProductOfferingMother.sample1().id());
+        externalEntitiesIdList.add(ProductOfferingMother.sample2().id());
+        return externalEntitiesIdList;
+    }
+}
