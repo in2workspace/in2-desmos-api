d634884a95b7b2d4fed7edf366b2629caccc5cc6
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-11 16:44:40 +0200

    fix(event): return mono void on entities creator listener

--- src/main/java/es/in2/desmos/domain/events/EntitiesCreatorEventListener.java
+++ src/main/java/es/in2/desmos/domain/events/EntitiesCreatorEventListener.java
@@ -1,10 +1,12 @@
 package es.in2.desmos.domain.events;
 
+import es.in2.desmos.domain.models.EntitiesCreatorEvent;
 import es.in2.desmos.domain.services.sync.NewEntitiesCreatorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
@@ -13,8 +15,8 @@ public class EntitiesCreatorEventListener {
     private final NewEntitiesCreatorService newEntitiesCreatorService;
 
     @EventListener
-    public void onApplicationEvent(EntitiesCreatorEvent event) {
+    public Mono<Void> onApplicationEvent(EntitiesCreatorEvent event) {
         log.info("Received entities creator event.");
-        newEntitiesCreatorService.addNewEntities(event.issuer(), event.externalEntityIds(), event.internalEntityIds());
+        return newEntitiesCreatorService.addNewEntities(event.issuer(), event.externalEntityIds(), event.internalEntityIds());
     }
 }
