e17101d3779eee60b80f17fed8c2663e2400cee9
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-11 12:42:49 +0200

    test(workflow): end test when should return missing external entities

--- src/test/java/es/in2/desmos/workflows/DiscoverySyncWorkflowIT.java
+++ src/test/java/es/in2/desmos/workflows/DiscoverySyncWorkflowIT.java
@@ -3,10 +3,12 @@ package es.in2.desmos.workflows;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import es.in2.desmos.ContainerManager;
-import es.in2.desmos.domain.models.ProductOffering;
+import es.in2.desmos.domain.models.DiscoverySyncRequest;
+import es.in2.desmos.domain.models.DiscoverySyncResponse;
 import es.in2.desmos.objectmothers.DiscoverySyncRequestMother;
 import es.in2.desmos.objectmothers.DiscoverySyncResponseMother;
 import es.in2.desmos.objectmothers.ProductOfferingMother;
+import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,51 +19,69 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DiscoverySyncWorkflowIT {
 
     @Autowired
-    private WebTestClient webTestClient;
-
-    @Autowired
     private ObjectMapper objectMapper;
+
+    @LocalServerPort
+    private int localServerPort;
 
     @Value("${broker.externalDomain}")
     private String contextBrokerExternalDomain;
 
     private final MediaType APPLICATION_LD_JSON = new MediaType("application", "ld+json");
 
+    @DynamicPropertySource
+    static void setDynamicProperties(DynamicPropertyRegistry registry) {
+        ContainerManager.postgresqlProperties(registry);
+    }
+
     @Test
-    void itShouldCreateLocalEntitiesBasedOnExternalEntitiesAndReturnMissingExternalEntities() throws JsonProcessingException, JSONException {
-        var discoverySyncRequest = DiscoverySyncRequestMother.simpleDiscoverySyncRequest();
+    void itShouldReturnMissingExternalEntities() throws JsonProcessingException, JSONException {
+        DiscoverySyncRequest discoverySyncRequest = DiscoverySyncRequestMother.list1And2();
+        Mono<DiscoverySyncRequest> discoverySyncRequestMono = Mono.just(discoverySyncRequest);
         String discoverySyncRequestJson = objectMapper.writeValueAsString(discoverySyncRequest);
 
-        var discoverySyncResponse = DiscoverySyncResponseMother.fullDiscoverySyncResponse(contextBrokerExternalDomain);
+        DiscoverySyncResponse discoverySyncResponse = DiscoverySyncResponseMother.list3And4(contextBrokerExternalDomain);
         String discoverySyncResponseJson = objectMapper.writeValueAsString(discoverySyncResponse);
 
-        String brokerUrl = ContainerManager.getBaseUriForScorpioA();
-        addInitialEntitiesToContextBroker(brokerUrl);
+        addInitialEntitiesToContextBroker(contextBrokerExternalDomain);
 
-        webTestClient.post()
+        String response = WebClient.builder()
+                .baseUrl("http://localhost:" + localServerPort)
+                .build()
+                .post()
                 .uri("/api/v1/sync/discovery")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(discoverySyncRequestJson)
-                .exchange()
-                .expectStatus().isAccepted()
-                .expectHeader().contentType(MediaType.APPLICATION_JSON)
-                .expectBody()
-                .json(discoverySyncResponseJson)
-                .consumeWith(System.out::println);
+                .body(discoverySyncRequestMono, DiscoverySyncRequest.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .retry(3).block();
+
+        System.out.println("Request: " + discoverySyncRequestJson);
+        System.out.println("Response: " +  discoverySyncResponseJson);
+
+        assertEquals(discoverySyncResponseJson, response);
+    }
+
+    @Test
+    void itShouldCreateLocalEntitiesBasedOnExternalEntities(){
+        //TODO
+        throw new NotImplementedException();
     }
 
     private void addInitialEntitiesToContextBroker(String brokerUrl) throws JsonProcessingException, JSONException {
@@ -83,7 +103,9 @@ class DiscoverySyncWorkflowIT {
     private String createInitialEntitiesRequestBody() throws JsonProcessingException, JSONException {
         JSONArray productOfferingsJsonArray = new JSONArray();
 
-        for (var productOffering : createInitialEntities()) {
+        var initialEntities = ProductOfferingMother.list3And4();
+
+        for (var productOffering : initialEntities) {
             var productOfferingJsonText = objectMapper.writeValueAsString(productOffering);
             var productOfferingJson = new JSONObject(productOfferingJsonText);
 
@@ -99,12 +121,5 @@ class DiscoverySyncWorkflowIT {
         String requestBody = productOfferingsJsonArray.toString();
         requestBody = requestBody.replace("\\/", "/");
         return requestBody;
-    }
-
-    private @NotNull List<ProductOffering> createInitialEntities() {
-        List<ProductOffering> initialEntities = new ArrayList<>();
-        initialEntities.add(ProductOfferingMother.sample3());
-        initialEntities.add(ProductOfferingMother.sample4());
-        return initialEntities;
     }
 }
