2a8ebeafd499d85c58c3870a6d041312ead761ed
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-12 14:09:22 +0200

    fix(model): remove entities creator event

--- src/main/java/es/in2/desmos/domain/models/EntitiesCreatorEvent.java
+++
@@ -1,9 +0,0 @@
-package es.in2.desmos.domain.models;
-
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-
-public record EntitiesCreatorEvent(Mono<String> issuer, Mono<List<String>> externalEntityIds,
-                                   Mono<List<String>> internalEntityIds) {
-}
