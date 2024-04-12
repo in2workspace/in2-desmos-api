7bb4431521dca8e46ac97a3aa59dc762bb595028
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-12 13:58:38 +0200

    test(service): add mocks and change assert by stepverifier

--- src/test/java/es/in2/desmos/workflows/P2PDataSyncWorkflowTests.java
+++ src/test/java/es/in2/desmos/workflows/P2PDataSyncWorkflowTests.java
@@ -1,30 +1,48 @@
 package es.in2.desmos.workflows;
 
+import es.in2.desmos.domain.events.DataNegotiationEventPublisher;
 import es.in2.desmos.domain.models.Entity;
+import es.in2.desmos.domain.services.broker.impl.BrokerEntityIdGetterServiceImpl;
 import es.in2.desmos.objectmothers.EntityMother;
 import es.in2.desmos.workflows.impl.P2PDataSyncWorkflowImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class P2PDataSyncWorkflowTests {
     @InjectMocks
-    P2PDataSyncWorkflowImpl p2PDataSyncWorkflow;
+    private P2PDataSyncWorkflowImpl p2PDataSyncWorkflow;
+
+    @Mock
+    private BrokerEntityIdGetterServiceImpl brokerEntityIdGetterService;
+
+    @Mock
+    private DataNegotiationEventPublisher dataNegotiationEventPublisher;
 
     @Test
     void itShouldReturnInternalEntities() {
 
         List<Entity> expectedInternalEntities = EntityMother.list3And4();
 
-        var result = p2PDataSyncWorkflow.dataDiscovery("0", Mono.just("https://example.org"), Mono.just(EntityMother.list1And2()));
+        when(brokerEntityIdGetterService.getData()).thenReturn(Mono.just(expectedInternalEntities));
 
-        assertEquals(expectedInternalEntities, result);
+        Mono<List<Entity>> result = p2PDataSyncWorkflow.dataDiscovery("0", Mono.just("https://example.org"), Mono.just(EntityMother.list1And2()));
+
+        StepVerifier.create(result)
+                .expectNext(expectedInternalEntities)
+                .verifyComplete();
+
+        verify(brokerEntityIdGetterService, times(1)).getData();
+        verifyNoMoreInteractions(brokerEntityIdGetterService);
+
     }
 }
