be64ce519224f690b46ca83d9f411a7716c0ff0d
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-12 13:41:52 +0200

    fix(service): Remove deprecated interface

--- src/main/java/es/in2/desmos/domain/services/sync/NewEntitiesCreatorService.java
+++
@@ -1,9 +0,0 @@
-package es.in2.desmos.domain.services.sync;
-
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-
-public interface NewEntitiesCreatorService {
-    Mono<Void> addNewEntities(Mono<String> issuer, Mono<List<String>> externalEntityIds, Mono<List<String>> internalEntityIds);
-}
