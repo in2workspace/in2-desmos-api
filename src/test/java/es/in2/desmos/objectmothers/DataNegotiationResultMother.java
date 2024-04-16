7831bc562c8d0f0f2aca1c31c85d207fb3446a48
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-16 17:10:45 +0200

    test(objectmother): create data negotiation result mother

---
+++ src/test/java/es/in2/desmos/objectmothers/DataNegotiationResultMother.java
@@ -0,0 +1,18 @@
+package es.in2.desmos.objectmothers;
+
+import es.in2.desmos.domain.models.DataNegotiationResult;
+import es.in2.desmos.domain.models.MVEntity4DataNegotiation;
+
+import java.util.List;
+
+public final class DataNegotiationResultMother {
+    private DataNegotiationResultMother() {
+    }
+
+    public static DataNegotiationResult sample(){
+        String issuer = "http://example.org";
+        List<MVEntity4DataNegotiation> newEntitiesToSync = MVEntity4DataNegotiationMother.list1And2();
+        List<MVEntity4DataNegotiation> existingEntitiesToSync = MVEntity4DataNegotiationMother.list3And4();
+        return new DataNegotiationResult(issuer, newEntitiesToSync, existingEntitiesToSync);
+    }
+}
