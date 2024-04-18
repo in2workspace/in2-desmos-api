475a9f47219f5111e567895be93b4d41e9204954
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-18 09:20:10 +0200

    test(objectmother): expand entity sync response mother sample

--- src/test/java/es/in2/desmos/objectmothers/EntitySyncResponseMother.java
+++ src/test/java/es/in2/desmos/objectmothers/EntitySyncResponseMother.java
@@ -1,43 +1,72 @@
 package es.in2.desmos.objectmothers;
 
-import es.in2.desmos.domain.models.EntitySyncResponse;
 import org.jetbrains.annotations.NotNull;
 
 public class EntitySyncResponseMother {
     private EntitySyncResponseMother() {
     }
 
-    public static @NotNull EntitySyncResponse sample() {
-        return new EntitySyncResponse(sampleJson);
+    public static @NotNull String sample() {
+        return """
+                [
+                  {
+                    "id": "urn:productOffering:d86735a6-0faa-463d-a872-00b97affa1cb",
+                    "type": "productOffering",
+                    "version": "1.2",
+                    "lastUpdate": "2024-09-05T12:00:00Z",
+                    "productSpecification": {
+                      "id": "spec-broadband-001",
+                      "name": "1Gbps Broadband Spec"
+                    },
+                    "productOfferingPrice": {
+                      "type": "Relationship",
+                      "object": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a"
+                    }
+                  },
+                  {
+                    "id": "urn:productOfferingPrice:ed9c56c8-a5ab-42cc-bc62-0fca69a30c87",
+                    "type": "productOffering",
+                    "version": "2.5",
+                    "lastUpdate": "2024-07-09T12:00:00Z",
+                    "productSpecification": {
+                      "id": "spec-broadband-001",
+                      "name": "1Gbps Broadband Spec"
+                    },
+                    "productOfferingPrice": {
+                      "type": "Relationship",
+                      "object": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a"
+                    }
+                  },
+                  {
+                    "id": "urn:productOfferingPrice:537e1ee3-0556-4fff-875f-e55bb97e7ab0",
+                    "type": "productOffering",
+                    "version": "4.3",
+                    "lastUpdate": "2024-04-03T12:00:00Z",
+                    "productSpecification": {
+                      "id": "spec-broadband-001",
+                      "name": "1Gbps Broadband Spec"
+                    },
+                    "productOfferingPrice": {
+                      "type": "Relationship",
+                      "object": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a"
+                    }
+                  },
+                  {
+                    "id": "urn:productOfferingPrice:3645a0de-d74f-42c5-86ab-e27ccbdf0a9c",
+                    "type": "productOffering",
+                    "version": "1.9",
+                    "lastUpdate": "2024-06-02T12:00:00Z",
+                    "productSpecification": {
+                      "id": "spec-broadband-001",
+                      "name": "1Gbps Broadband Spec"
+                    },
+                    "productOfferingPrice": {
+                      "type": "Relationship",
+                      "object": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a"
+                    }
+                  }
+                ]
+                """;
     }
-
-    private static final String sampleJson =
-            """
-                    [
-                      {
-                        "id": "urn:productOffering:537e1ee3-0556-4fff-875f-e55bb97e7ab0",
-                        "type": "productOffering",
-                        "productSpecification": {
-                          "id": "spec-broadband-001",
-                          "name": "1Gbps Broadband Spec"
-                        },
-                        "productOfferingPrice": {
-                          "type": "Relationship",
-                          "object": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a"
-                        }
-                      },
-                      {
-                        "id": "urn:productOfferingPrice:912efae1-7ff6-4838-89f3-cfedfdfa1c5a",
-                        "type": "productOfferingPrice",
-                        "name": "Monthly Subscription Fee",
-                        "priceType": "recurring",
-                        "price": {
-                          "amount": "49.99",
-                          "currency": "USD"
-                        },
-                        "recurringChargePeriod": "monthly"
-                      }
-                    ]
-                    """;
 
 }
