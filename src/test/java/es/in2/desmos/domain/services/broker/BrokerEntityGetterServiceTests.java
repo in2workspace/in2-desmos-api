6d01be3aa9145d429f28826fb39b5250b575d21e
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-18 13:43:58 +0200

    test(service): rename service test to adapt to the service name

--- src/test/java/es/in2/desmos/domain/services/broker/BrokerEntityGetterServiceTests.java
+++ src/test/java/es/in2/desmos/domain/services/broker/BrokerEntityGetterServiceTests.java
@@ -23,7 +23,7 @@ import java.util.List;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class BrokerMVEntity4DataNegotiationGetterServiceTests {
+class BrokerEntityGetterServiceTests {
 
     @Autowired
     private ObjectMapper objectMapper;
