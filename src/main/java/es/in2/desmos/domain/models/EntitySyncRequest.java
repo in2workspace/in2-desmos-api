5d24bd570e96d8d8a85b3c80f0c4c5096e1ee21f
Albert Rodríguez <albert.rodriguez@in2.es>
2024-04-18 09:17:14 +0200

    feat(model): create entity sync request and override base methods

--- src/main/java/es/in2/desmos/domain/models/EntitySyncRequest.java
+++ src/main/java/es/in2/desmos/domain/models/EntitySyncRequest.java
@@ -1,6 +1,25 @@
 package es.in2.desmos.domain.models;
 
-import java.util.List;
+import java.util.Arrays;
 
-public record EntitySyncRequest(List<MVEntity4DataNegotiation> entities) {
+public record EntitySyncRequest(MVEntity4DataNegotiation[] entities) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntitySyncRequest entitySyncRequest = (EntitySyncRequest) o;
+        return Arrays.equals(entities, entitySyncRequest.entities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(entities);
+    }
+
+    @Override
+    public String toString() {
+        return "EntitySyncRequest{" +
+                "entities=" + Arrays.toString(entities) +
+                '}';
+    }
 }
