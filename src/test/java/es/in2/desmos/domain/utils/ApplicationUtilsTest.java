7b39ec5acf6d8bba622c56a571d19668c9f2ec35
Juan Martin Gallo <juan.gallo@in2.es>
2024-04-19 10:13:43 +0200

    New tests for the new code

---
+++ src/test/java/es/in2/desmos/domain/utils/ApplicationUtilsTest.java
@@ -0,0 +1,33 @@
+package es.in2.desmos.domain.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationUtilsTest {
+    @Test
+    void testGetEnvironmentMetadataWithValidProfiles() {
+        // Arrange & Act
+        String localMetadata = ApplicationUtils.getEnvironmentMetadata("default");
+        String sbxMetadata = ApplicationUtils.getEnvironmentMetadata("dev");
+        String devMetadata = ApplicationUtils.getEnvironmentMetadata("test");
+        String prdMetadata = ApplicationUtils.getEnvironmentMetadata("prod");
+
+        // Assert.assertEquals("local", localMetadata);
+        assertEquals("sbx", sbxMetadata);
+        assertEquals("dev", devMetadata);
+        assertEquals("prd", prdMetadata);
+    }
+
+    @Test
+    void testGetEnvironmentMetadataWithInvalidProfile() {
+        // Arrange & Act & Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            ApplicationUtils.getEnvironmentMetadata("invalid");
+        });
+    }
+}
