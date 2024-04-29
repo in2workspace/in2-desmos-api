6e1847eeb85b4f4e340e018d60aac9e19e9fd1d5
Oriol Canadés <oriol.canades@in2.es>
2024-04-29 08:37:48 +0200

    Update dependency maintenance. Delete classes are not used anymore.

--- src/main/java/es/in2/desmos/domain/services/api/BrokerEntityVerifyService.java
+++
@@ -1,8 +0,0 @@
-//package es.in2.desmos.domain.services.api;
-//
-//import es.in2.desmos.domain.models.BlockchainNotification;
-//import reactor.core.publisher.Mono;
-//
-//public interface BrokerEntityVerifyService {
-//    Mono<String> verifyRetrievedEntityDataIntegrity(String processId, BlockchainNotification blockchainNotification, String retrievedBrokerEntity);
-//}
