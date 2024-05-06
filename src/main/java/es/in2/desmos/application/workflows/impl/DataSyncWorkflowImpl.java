8e805cbf983011b6229157e78a7888e0cfdb0ef4
Albert Rodríguez <albert.rodriguez@in2.es>
2024-05-06 15:28:47 +0200

    fix(workflow): remove unused attribute

--- src/main/java/es/in2/desmos/application/workflows/impl/DataSyncWorkflowImpl.java
+++ src/main/java/es/in2/desmos/application/workflows/impl/DataSyncWorkflowImpl.java
@@ -2,7 +2,6 @@ package es.in2.desmos.application.workflows.impl;
 
 import es.in2.desmos.application.workflows.DataSyncWorkflow;
 import es.in2.desmos.domain.services.sync.jobs.BlockchainDataSyncJob;
-import es.in2.desmos.domain.services.sync.jobs.P2PDataSyncJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,7 +13,6 @@ import reactor.core.publisher.Flux;
 public class DataSyncWorkflowImpl implements DataSyncWorkflow {
 
     private final BlockchainDataSyncJob blockchainDataSyncJob;
-    private final P2PDataSyncJob p2PDataSyncJob;
 
     @Override
     public Flux<Void> startDataSyncWorkflow(String processId) {
