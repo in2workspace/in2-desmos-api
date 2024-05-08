91f2c4c722dc45327c0e353d5d2e4d7f9a06e600
Albert Rodríguez <albert.rodriguez@in2.es>
2024-05-08 09:22:35 +0200

    fix(config): fix package name

--- src/main/java/es/in2/desmos/infrastructure/configs/AsynchronousSpringEventsConfig.java
+++ src/main/java/es/in2/desmos/infrastructure/configs/AsynchronousSpringEventsConfig.java
@@ -1,4 +1,4 @@
-package es.in2.desmos.configs;
+package es.in2.desmos.infrastructure.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
