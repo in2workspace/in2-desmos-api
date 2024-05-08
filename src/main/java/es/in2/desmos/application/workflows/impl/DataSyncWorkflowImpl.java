55c0181d6ac9ece86afdfb9b6d54b3b435a9cd7e
Albert Rodríguez <albert.rodriguez@in2.es>
2024-05-08 09:34:20 +0200

    fix(workflow): change from calling to blochchain sync to p2p data sync

--- src/main/java/es/in2/desmos/application/workflows/impl/DataSyncWorkflowImpl.java
+++ src/main/java/es/in2/desmos/application/workflows/impl/DataSyncWorkflowImpl.java
@@ -1,7 +1,7 @@
 package es.in2.desmos.application.workflows.impl;
 
 import es.in2.desmos.application.workflows.DataSyncWorkflow;
-import es.in2.desmos.domain.services.sync.jobs.BlockchainDataSyncJob;
+import es.in2.desmos.domain.services.sync.jobs.P2PDataSyncJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,11 +12,17 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class DataSyncWorkflowImpl implements DataSyncWorkflow {
 
-    private final BlockchainDataSyncJob blockchainDataSyncJob;
+    @SuppressWarnings({"GrazieInspection", "java:S125"})
+    // private final BlockchainDataSyncJob blockchainDataSyncJob;
+
+    private final P2PDataSyncJob p2PDataSyncJob;
 
     @Override
     public Flux<Void> startDataSyncWorkflow(String processId) {
-        // TODO: Change blockchainDataSyncService to p2pDataSyncService in future
-        return blockchainDataSyncJob.startBlockchainDataSyncJob(processId);
+        // TODO: We have not yet decided which job will be applied, so for the moment we will keep both options in the code
+
+        // return blockchainDataSyncJob.startBlockchainDataSyncJob(processId); // NOSONAR
+
+        return p2PDataSyncJob.synchronizeData(processId).flux();
     }
 }
