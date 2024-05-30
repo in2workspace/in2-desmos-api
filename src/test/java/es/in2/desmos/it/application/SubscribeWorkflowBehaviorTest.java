37eba48b76c014d4e604a89c8161ef313e01a123
yaniaiciin2 <yani.aici@in2.es>
2024-05-30 09:13:05 +0200

    Update SubscribeWorkflowBehaviorTest.java

--- src/test/java/es/in2/desmos/it/application/SubscribeWorkflowBehaviorTest.java
+++ src/test/java/es/in2/desmos/it/application/SubscribeWorkflowBehaviorTest.java
@@ -68,6 +68,20 @@ class SubscribeWorkflowBehaviorTest {
                 }
             }""";
 
+    String updateRequest = """
+            {
+                "id": "urn:ngsi-ld:ProductOffering:122355255",
+                "type": "ProductOffering",
+                "name": {
+                    "type": "Property",
+                    "value": "ProductOffering 2"
+                },
+                "description": {
+                    "type": "Property",
+                    "value": "ProductOffering 2 description"
+                }
+            }""";
+
     /*
         Given a BlockchainNotification, we will send a POST request emulating the broker behavior.
         When the POST request is received, the application will retrieve a BrokerEntity from the external broker,
@@ -97,6 +111,48 @@ class SubscribeWorkflowBehaviorTest {
                     "eventType": "ProductOffering",
                     "timestamp": 1712753824,
                     "dataLocation": "%s/ngsi-ld/v1/entities/urn:ngsi-ld:ProductOffering:122355255?hl=abbc168236d38354add74d65698f37941947127290cd40a90b4dbe7eb68d25c0",
+                    "relevantMetadata": [],
+                    "entityId": "0x4eb401aa1248b6a95c298d0747eb470b6ba6fc3f54ea630dc6c77f23ad1abe3e",
+                    "previousEntityHash": "0xabbc168236d38354add74d65698f37941947127290cd40a90b4dbe7eb68d25c0"
+                }""", url);
+        // When
+        try {
+            log.info("1. Create a BlockchainNotification and send a POST request to the application");
+            BlockchainNotification blockchainNotification = objectMapper.readValue(blockchainNotificationJson, BlockchainNotification.class);
+            StepVerifier.create(notificationController.postDLTNotification(blockchainNotification))
+                    .verifyComplete();
+            log.info("1.1. Get the event stream from the pendingSubscribeQueue and subscribe to it.");
+            pendingSubscribeEventsQueue.getEventStream().subscribe(event -> log.info("Event: {}", event));
+            log.info("2. Check values in the AuditRecord table:");
+            List<AuditRecord> auditRecordList = auditRecordRepository.findAll().collectList().block();
+            log.info("Result: {}", auditRecordList);
+        } catch (Exception e) {
+            log.error("Error while sending the BlockchainNotification: {}", e.getMessage());
+        }
+    }
+
+    @Order(1)
+    @Test
+    void subscribeWorkflowBehaviorTest_UPDATE() {
+        log.info("Starting Subscribe Workflow Behavior Test...");
+        log.info("2. Send a POST request to the external broker in order to retrieve the entity later");
+        WebClient.create().patch()
+                .uri(ContainerManager.getBaseUriForScorpioA() + "/ngsi-ld/v1/entities")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(updateRequest)
+                .retrieve()
+                .onStatus(status -> status.isSameCodeAs(HttpStatusCode.valueOf(409)), response -> Mono.empty())
+                .bodyToMono(String.class).block();
+
+        String url = ContainerManager.getBaseUriForScorpioA();
+        String blockchainNotificationJson = String.format("""
+                {
+                    "id": 2240,
+                    "publisherAddress": "0x40b0ab9dfd960064fb7e9fdf77f889c71569e349055ff563e8d699d8fa97fa90",
+                    "eventType": "ProductOffering",
+                    "timestamp": 1712753824,
+                    "dataLocation": "%s/ngsi-ld/v1/entities/urn:ngsi-ld:ProductOffering:122355255?hl=64f516030ab35b7f18d8d06054079e85070ea04f1c0dab5291be8184a28025fa",
                     "relevantMetadata": [],
                     "entityId": "0x4eb401aa1248b6a95c298d0747eb470b6ba6fc3f54ea630dc6c77f23ad1abe3e",
                     "previousEntityHash": "0xabbc168236d38354add74d65698f37941947127290cd40a90b4dbe7eb68d25c0"
