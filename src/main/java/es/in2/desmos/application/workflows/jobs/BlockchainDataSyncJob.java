510f928451638512f3444699420ebc538faee8e0
Albert Rodríguez <albert.rodriguez@in2.es>
2024-07-24 18:20:04 +0200

    Removed duplicate job

--- src/main/java/es/in2/desmos/application/workflows/jobs/BlockchainDataSyncJob.java
+++
@@ -1,7 +0,0 @@
-package es.in2.desmos.application.workflows.jobs;
-
-import reactor.core.publisher.Flux;
-
-public interface BlockchainDataSyncJob {
-    Flux<Void> startBlockchainDataSyncJob(String processId);
-}
