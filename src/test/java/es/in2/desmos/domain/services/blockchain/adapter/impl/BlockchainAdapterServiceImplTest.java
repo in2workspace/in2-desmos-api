25ebb7d61db3915ef07ffe49d030c80dfd39f133
Albert Rodríguez <albert.rodriguez@in2.es>
2024-09-14 17:44:39 +0200

    test(subscriptions): test get subscriptions

--- src/test/java/es/in2/desmos/domain/services/blockchain/adapter/impl/BlockchainAdapterServiceImplTest.java
+++ src/test/java/es/in2/desmos/domain/services/blockchain/adapter/impl/BlockchainAdapterServiceImplTest.java
@@ -1,11 +1,50 @@
 package es.in2.desmos.domain.services.blockchain.adapter.impl;
 
+import es.in2.desmos.domain.models.BlockchainSubscription;
+import es.in2.desmos.domain.services.blockchain.adapter.BlockchainAdapterService;
+import es.in2.desmos.it.ContainerManager;
+import es.in2.desmos.objectmothers.BlockchainSubscriptionMother;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
 class BlockchainAdapterServiceImplTest {
 
-    //TODO
+    @Autowired
+    private BlockchainAdapterService blockchainAdapterService;
+
+    @DynamicPropertySource
+    static void setDynamicProperties(DynamicPropertyRegistry registry) {
+        ContainerManager.postgresqlProperties(registry);
+    }
+
     @Test
-    void getSubscriptions() {
+    void itShouldGetSubscriptions() {
+        BlockchainSubscription expected1 = BlockchainSubscriptionMother.sample();
+        BlockchainSubscription expected2 = BlockchainSubscriptionMother.otherEventTypesSubscription();
+
+        createSubscriptions(expected1, expected2);
+
+        var resultMono = blockchainAdapterService.getSubscriptions("0");
+
+        StepVerifier
+                .create(resultMono)
+                .assertNext(result -> assertThat(result).isEqualTo(expected1))
+                .assertNext(result -> assertThat(result).isEqualTo(expected2))
+                .verifyComplete();
+    }
+
+    private void createSubscriptions(BlockchainSubscription... blockchainSubscriptions) {
+        for (var subscription : blockchainSubscriptions) {
+            blockchainAdapterService.createSubscription("0", subscription).block();
+        }
     }
 }
