3ebde71018907d4ff48251e62021d2e74e6e639a
Oriol Canadés <oriol.canades@in2.es>
2024-10-18 08:43:16 +0200

    Update BrokerEntityValidFor.java

--- src/main/java/es/in2/desmos/domain/models/BrokerEntityValidFor.java
+++ src/main/java/es/in2/desmos/domain/models/BrokerEntityValidFor.java
@@ -3,6 +3,6 @@ package es.in2.desmos.domain.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
-public record BrokerEntityValidFor(@JsonProperty("startDateTime")
-                                   @NotBlank String startDateTime
+public record BrokerEntityValidFor(
+  @JsonProperty("startDateTime") @NotBlank String startDateTime
 ) {}
