24055636ea7766f1f97737066c71cc1b84fc9214
Oriol Canadés <oriol.canades@in2.es>
2024-11-05 09:30:43 +0100

    Clean repository of unnecessary files

--- src/main/java/es/in2/desmos/domain/repositories/AuditRecordRepository.java
+++ src/main/java/es/in2/desmos/domain/repositories/AuditRecordRepository.java
@@ -37,9 +37,6 @@ public interface AuditRecordRepository extends ReactiveCrudRepository<AuditRecor
     @Query("SELECT * FROM audit_records WHERE entity_id = :entityId AND status = 'PUBLISHED' ORDER BY created_at DESC LIMIT 1")
     Mono<AuditRecord> findMostRecentPublishedAuditRecordByEntityId(String entityId);
 
-    @Query("SELECT * FROM audit_records WHERE entity_id = :entityId AND status = 'PUBLISHED' ORDER BY created_at DESC LIMIT 1")
-    Mono<AuditRecord> findMostRecentPublishedAuditRecordByEntityId(String entityId);
-
     @Query("SELECT * FROM audit_records WHERE entity_id = :entityId AND status = 'PUBLISHED' AND trader = 'PRODUCER' ORDER BY created_at DESC LIMIT 1")
     Mono<AuditRecord> findLatestPublishedAuditRecordForProducerByEntityId(String entityId);
 
