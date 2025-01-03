d5f502e3d932956f8f2979f1e0ece25603986171
Albert Rodríguez <albert.rodriguez@in2.es>
2025-01-03 12:52:30 +0100

    fix(service): remove entity data from broker notification info log

--- src/main/java/es/in2/desmos/infrastructure/controllers/NotificationController.java
+++ src/main/java/es/in2/desmos/infrastructure/controllers/NotificationController.java
@@ -6,12 +6,15 @@ import es.in2.desmos.domain.services.blockchain.BlockchainListenerService;
 import es.in2.desmos.domain.services.broker.BrokerListenerService;
 import es.in2.desmos.infrastructure.security.JwtTokenProvider;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
@@ -28,7 +31,9 @@ public class NotificationController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public Mono<Void> postBrokerNotification(@RequestBody @Valid BrokerNotification brokerNotification) {
         String processId = UUID.randomUUID().toString();
-        log.info("ProcessID: {} - Broker Notification received: {}", processId, brokerNotification.toString());
+        String id = getIdFromDataMap(brokerNotification.data());
+        log.info("ProcessID: {} - Broker Notification received for entity with id: {}", processId, id);
+        log.debug("ProcessID: {} - Broker Notification received: {}", processId, brokerNotification.toString());
         return brokerListenerService.processBrokerNotification(processId, brokerNotification);
     }
 
@@ -39,6 +44,10 @@ public class NotificationController {
         log.info("ProcessID: {} - Blockchain Notification received with DataLocation: {}", processId, blockchainNotification.dataLocation());
         log.debug("ProcessID: {}, Blockchain Notification received: {}", processId, blockchainNotification);
         return blockchainListenerService.processBlockchainNotification(processId, blockchainNotification);
+    }
+
+    private String getIdFromDataMap(@NotNull(message = "data cannot be null") List<Map<String, Object>> data) {
+        return data.get(0).get("id").toString();
     }
 
 }
