0ced4f3d6b94f1e4e367a622b971f39b05198e19
Albert Rodríguez <albert.rodriguez@in2.es>
2025-01-13 15:31:47 +0100

    fix(workflow): subtract time to start date time

--- src/main/java/es/in2/desmos/domain/services/policies/impl/ReplicationPoliciesServiceImpl.java
+++ src/main/java/es/in2/desmos/domain/services/policies/impl/ReplicationPoliciesServiceImpl.java
@@ -67,7 +67,7 @@ public class ReplicationPoliciesServiceImpl implements ReplicationPoliciesServic
 
     private boolean isAfterStartDateTime(String startDateTime) {
         return startDateTime == null ||
-                Instant.now().isAfter(Instant.parse(startDateTime));
+                Instant.now().minusSeconds(1).isAfter(Instant.parse(startDateTime));
     }
 
     private boolean isBeforeEndDateTime(String endDateTime) {
